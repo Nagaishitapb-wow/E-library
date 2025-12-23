37cbdeea07aa6735c2c7948e1b7a7d1c6671d2a4
Nagaishitapb-wow <ishita.pb@wowlabz.com>
2025-12-23 17:17:04 +0530

    fix:cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,7 +21,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
